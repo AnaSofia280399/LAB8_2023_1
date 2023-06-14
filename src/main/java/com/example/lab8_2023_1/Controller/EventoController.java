4da95da5b7c4165733376f310e0b8cf5048f640d
Ana Sofia <alejandro.as@pucp.edu.pe>
2023-06-13 21:07:44 -0500

    1.b) obtener por id

--- src/main/java/com/example/lab8_2023_1/Controller/EventoController.java
+++ src/main/java/com/example/lab8_2023_1/Controller/EventoController.java
@@ -2,12 +2,12 @@ package com.example.lab8_2023_1.Controller;
 
 import com.example.lab8_2023_1.Entity.Evento;
 import com.example.lab8_2023_1.Repository.EventoRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -25,4 +25,27 @@ public class EventoController {
         List<Evento> list = eventoRepository.findAll();
         return  list;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HashMap<String,Object >>  obtenerEventoPorId(@PathVariable("id") String idStr) {
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+        try {
+            Optional<Evento> optionalEvento = eventoRepository.findById(Integer.parseInt(idStr));
+            if (optionalEvento.isPresent()) {
+                responseJson.put("evento", optionalEvento.get());
+                responseJson.put("resultado", "exitoso");
+                return ResponseEntity.ok(responseJson);
+            } else {
+                responseJson.put("msg", "Evento no encontrado");
+
+            }
+        } catch (NumberFormatException e) {
+            responseJson.put("msg", "El ID debe ser un numero entero positivo");
+        }
+        responseJson.put("resultado", "Falla");
+        return ResponseEntity.badRequest().body(responseJson);
+    }
+
+
 }
